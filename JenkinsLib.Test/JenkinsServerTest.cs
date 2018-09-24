9be0c43b125637af3e3553ed508ea5155e920584
Hayato Iriumi <hiriumi@gmail.com>
2018-09-23 22:45:14 -0700

    saving

--- JenkinsLib.Test/JenkinsServerTest.cs
+++ JenkinsLib.Test/JenkinsServerTest.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace JenkinsLib.Test
 {
    
     public class JenkinsServerTest
     {
+        [Fact]
+        public void Hoge()
+        {
+            Console.WriteLine("Hello World");
+        }
     }
 }
