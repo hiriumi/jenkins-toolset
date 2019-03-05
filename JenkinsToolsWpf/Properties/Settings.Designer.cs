7c11af7fc51bf2fec75b93463f7bc6e70754b5fb
Hayato Iriumi <hayato.iriumi@costcotravel.com>
2019-03-05 15:34:55 -0800

    Changed the default for ValidateJenkinsJobXmlWellFormed to False because of XML 1.1 validation issue.

--- JenkinsToolsWpf/Properties/Settings.Designer.cs
+++ JenkinsToolsWpf/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace JenkinsToolsetWpf.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -307,7 +307,7 @@ namespace JenkinsToolsetWpf.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool ValidateJenkinsJobXmlWellFormed {
             get {
                 return ((bool)(this["ValidateJenkinsJobXmlWellFormed"]));
