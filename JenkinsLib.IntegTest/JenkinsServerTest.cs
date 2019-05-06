26fe98092ff33ad7bebfe533d338a4f76d3838aa
Hayato Iriumi <hiriumi@gmail.com>
2019-05-06 11:19:13 -0700

    saving

---
+++ JenkinsLib.IntegTest/JenkinsServerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JenkinsLib.IntegTest
+{
+    [TestClass]
+    public class JenkinsServerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
